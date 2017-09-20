9e84767ef1b215095d18c50b0a46e0cd539b56df
Clecio Varjao <clecio.varjao@gmail.com>
2017-09-19 21:16:49 -0700

    review

--- research/OrientDB/model.sql
+++ research/OrientDB/model.sql
@@ -1,25 +1,41 @@
-connect remote:localhost/cmdb user pass
+--connect remote:localhost/cmdb user pass
+-- LOAD SCRIPT C:/Data/projects/dcts/research/OrientDB/model.sql
 
-/*Delete ALL data (vertex/edge)*/
-DELETE VERTEX V
-DELETE EDGE E
+-- QUERIES:
 
-/*Drop all (user) classes*/
-js; var classes=db.metadata.schema.getClass('E').getAllSubclasses().iterator(); while (classes.hasNext()) {db.query('DROP CLASS '+classes.next().getName());};end;
-js; var classes=db.metadata.schema.getClass('V').getAllSubclasses().iterator(); while (classes.hasNext()) {db.query('DROP CLASS '+classes.next().getName());};end;
+-- List the all nodes (paths) affected by 'SERVERA_I1'
+-- select from (TRAVERSE in() FROM (select * from `PhysicalServer` where key = "SERVERA_I1")) 
+-- List all LogicalComponent affected by 'SERVERA_I1'
+-- select from (TRAVERSE in() FROM (select * from `PhysicalServer` where key = "SERVERA_I1")) where @class='LogicalComponent'
+
+-- select from (TRAVERSE out('Manifests') FROM (select * from `Artifact` where key = "TITAN_JDBC")) 
+
+-- Impacted components when an artifact is Updated/Deployed
+--select from ( TRAVERSE in() FROM (select from (TRAVERSE out('Manifests') FROM (select * from `Artifact` where key = "TITAN_JDBC")) where @class='LogicalComponent'))
 
 
-CREATE CLASS Component IF NOT EXISTS EXTENDS V 
+--Delete ALL data (vertex/edge)
+DELETE VERTEX V;
+DELETE EDGE E;
 
+--Drop all (user) classes
+js; function dropClass(oclass, bdel){  var oclasses=oclass.getSubclasses().iterator();  while (oclasses.hasNext()) {    dropClass(oclasses.next(), true);  };  if (bdel){print("Dropping '"+oclass.getName()+"'");  db.executeCommand("DROP CLASS "+oclass.getName());};}; dropClass(db.metadata.schema.getClass('E'), false); dropClass(db.metadata.schema.getClass('V'), false);end;
+
+
+CREATE CLASS Component IF NOT EXISTS EXTENDS V
+CREATE CLASS Environment IF NOT EXISTS EXTENDS V
 CREATE CLASS ExecutionEnvironment IF NOT EXISTS EXTENDS V 
 CREATE CLASS Artifact IF NOT EXISTS EXTENDS V 
 CREATE CLASS DeploymentSpec IF NOT EXISTS EXTENDS V 
-CREATE CLASS DeviceNode IF NOT EXISTS EXTENDS V 
+CREATE CLASS DeviceNode IF NOT EXISTS EXTENDS V
+CREATE CLASS VirtualServer IF NOT EXISTS EXTENDS DeviceNode
+CREATE CLASS PhysicalServer IF NOT EXISTS EXTENDS DeviceNode
 CREATE CLASS DeploymentSpec IF NOT EXISTS EXTENDS V 
 CREATE CLASS PropertyValue IF NOT EXISTS EXTENDS V 
 CREATE CLASS PropertyName IF NOT EXISTS EXTENDS V 
 CREATE CLASS LogicalComponent IF NOT EXISTS EXTENDS Component 
-CREATE CLASS PhysicalComponent IF NOT EXISTS EXTENDS Component 
+CREATE CLASS PhysicalComponent IF NOT EXISTS EXTENDS Component
+CREATE CLASS BundleLogicalComponent IF NOT EXISTS EXTENDS LogicalComponent  
 CREATE CLASS LogicalExecutionEnvironment IF NOT EXISTS EXTENDS ExecutionEnvironment 
 CREATE CLASS PhysicalExecutionEnvironment IF NOT EXISTS EXTENDS ExecutionEnvironment 
 CREATE CLASS LogicalDeploymentSpec IF NOT EXISTS EXTENDS DeploymentSpec 
@@ -27,85 +43,114 @@ CREATE CLASS PhysicalDeploymentSpec IF NOT EXISTS EXTENDS DeploymentSpec
 
 CREATE CLASS Delivery_Instance IF NOT EXISTS EXTENDS E 
 CREATE CLASS Manifested_By IF NOT EXISTS EXTENDS E 
+CREATE CLASS Manifests IF NOT EXISTS EXTENDS E 
 CREATE CLASS Deployed_To IF NOT EXISTS EXTENDS E 
 CREATE CLASS Instance_Of IF NOT EXISTS EXTENDS E 
+CREATE CLASS Has_Instance IF NOT EXISTS EXTENDS E 
 CREATE CLASS Has IF NOT EXISTS EXTENDS E 
 CREATE CLASS Hosted_By IF NOT EXISTS EXTENDS E 
+CREATE CLASS Uses IF NOT EXISTS EXTENDS E 
+CREATE CLASS Is_Used_By IF NOT EXISTS EXTENDS E 
+
+CREATE VERTEX Environment SET key = "INTEGRATION"
+CREATE VERTEX Environment SET key = "TEST"
+CREATE VERTEX Environment SET key = "PRODUCTION"
 
 -- Infrastructure
-CREATE VERTEX DeviceNode SET key = "BLEWIT"
-CREATE VERTEX DeviceNode SET key = "SERVER2"
+CREATE VERTEX PhysicalServer SET key = "SERVERA_I1"
+CREATE VERTEX PhysicalServer SET key = "SERVERA_T1"
+CREATE VERTEX PhysicalServer SET key = "SERVERA_P1"
+CREATE VERTEX VirtualServer  SET key = "SERVERB_1"
+CREATE VERTEX VirtualServer  SET key = "SERVERB_2"
 
 -- Middle Tier
-CREATE VERTEX  LogicalExecutionEnvironment SET key = "WLH01"
-CREATE VERTEX  PhysicalExecutionEnvironment SET key = "DELIVERY_WLH01"
-CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLH01") TO (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_WLH01")
-CREATE EDGE Hosted_By FROM (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_WLH01") TO (SELECT FROM DeviceNode WHERE key = "BLEWIT")
+CREATE VERTEX  LogicalExecutionEnvironment SET key = "WLS", name = "Oracle WebLogic"
+CREATE VERTEX  LogicalExecutionEnvironment SET key = "WLS1"
+CREATE VERTEX  PhysicalExecutionEnvironment SET key = "WLS1I"
+CREATE VERTEX  PhysicalExecutionEnvironment SET key = "WLS1T"
+CREATE VERTEX  PhysicalExecutionEnvironment SET key = "WLS1P"
 
--- Application: IRS
-CREATE VERTEX LogicalComponent SET key = "IRS"
-CREATE VERTEX PhysicalComponent SET key = "DELIVERY_IRS"
-CREATE VERTEX  Artifact SET key = "IRS_EAR"
-CREATE VERTEX  LogicalDeploymentSpec SET key = "IRS_DEPLOYMENTSPEC"
-CREATE VERTEX  PhysicalDeploymentSpec SET key = "DELIVERY_IRS_DEPLOYMENTSPEC"
+CREATE VERTEX  LogicalExecutionEnvironment SET key = "TC", name = "Tomcat"
+CREATE VERTEX  LogicalExecutionEnvironment SET key = "IIS", name = "IIS"
 
-CREATE EDGE Delivery_Instance FROM (SELECT FROM COMPONENT WHERE key = "IRS") TO (SELECT FROM COMPONENT WHERE key = "DELIVERY_IRS")
-CREATE EDGE Manifested_By FROM (SELECT FROM COMPONENT WHERE key = "IRS") TO (SELECT FROM Artifact WHERE key = "IRS_EAR")
-CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "IRS_EAR") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLH01")
-CREATE EDGE Deployed_To FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_IRS") TO (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_WLH01")
-CREATE EDGE Has FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_IRS") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_IRS_DEPLOYMENTSPEC")
-CREATE EDGE Instance_Of FROM (Select FROM DeploymentSpec WHERE key = "IRS_DEPLOYMENTSPEC") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_IRS_DEPLOYMENTSPEC")
-CREATE EDGE Deployed_To FROM (Select FROM Artifact WHERE key = "IRS_EAR") TO (Select FROM DeploymentSpec WHERE key = "IRS_DEPLOYMENTSPEC")
 
--- Application: VMAD
+CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLS")
 
-CREATE VERTEX LogicalComponent SET key = "VMAD"
-CREATE VERTEX PhysicalComponent SET key = "DELIVERY_VMAD"
-CREATE VERTEX  Artifact SET key = "VMAD_EAR"
-CREATE VERTEX  DeploymentSpec SET key = "DELIVERY_VMAD_DEPLOYMENTSPEC"
-CREATE VERTEX  DeploymentSpec SET key = "VMAD_DEPLOYMENTSPEC"
+--CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1I") TO (SELECT FROM Environment WHERE key = "INTEGRATION")
+CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1I") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLS1")
 
-CREATE EDGE Delivery_Instance FROM (SELECT FROM COMPONENT WHERE key = "VMAD") TO (SELECT FROM COMPONENT WHERE key = "DELIVERY_VMAD")
-CREATE EDGE Manifested_By FROM (SELECT FROM COMPONENT WHERE key = "VMAD") TO (SELECT FROM Artifact WHERE key = "VMAD_EAR")
-CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "VMAD_EAR") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLH01")
-CREATE EDGE Deployed_To FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_VMAD") TO (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_WLH01")
-CREATE EDGE Has FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_VMAD") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_VMAD_DEPLOYMENTSPEC")
-CREATE EDGE Instance_Of FROM (Select FROM DeploymentSpec WHERE key = "VMAD_DEPLOYMENTSPEC") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_VMAD_DEPLOYMENTSPEC")
-CREATE EDGE Deployed_To FROM (Select FROM Artifact WHERE key = "VMAD_EAR") TO (Select FROM DeploymentSpec WHERE key = "VMAD_DEPLOYMENTSPEC")
+--CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1T") TO (SELECT FROM Environment WHERE key = "TEST")
+CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1T") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLS1")
 
--- Application: TITAN
+--CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1P") TO (SELECT FROM Environment WHERE key = "PRODUCTION")
+CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1P") TO (SELECT FROM ExecutionEnvironment WHERE key = "WLS1")
 
-CREATE VERTEX DeviceNode SET key = "SERVER1"
-CREATE VERTEX DeviceNode SET key = "SERVER2"
+CREATE EDGE Deployed_To FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1I") TO (SELECT FROM DeviceNode WHERE key = "SERVERA_I1")
+CREATE EDGE Deployed_To FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1T") TO (SELECT FROM DeviceNode WHERE key = "SERVERA_T1")
+CREATE EDGE Deployed_To FROM (SELECT FROM ExecutionEnvironment WHERE key = "WLS1P") TO (SELECT FROM DeviceNode WHERE key = "SERVERA_P1")
 
-CREATE VERTEX  LogicalExecutionEnvironment SET key = "TLB01"
-CREATE VERTEX  PhysicalExecutionEnvironment SET key = "DELIVERY_TLB01"
-CREATE EDGE Instance_Of FROM (SELECT FROM ExecutionEnvironment WHERE key = "TLB01") TO (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_TLB01")
-CREATE EDGE Hosted_By FROM (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_TLB01") TO (SELECT FROM DeviceNode WHERE key = "SERVER1")
-CREATE EDGE Hosted_By FROM (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_TLB01") TO (SELECT FROM DeviceNode WHERE key = "SERVER2")
 
-CREATE VERTEX LogicalComponent SET key = "TITAN"
-CREATE VERTEX PhysicalComponent SET key = "DELIVERY_TITAN"
+
+--Artifacts
 CREATE VERTEX  Artifact SET key = "TITAN_EAR"
-CREATE VERTEX  DeploymentSpec SET key = "DELIVERY_TITAN_DEPLOYMENTSPEC"
-CREATE VERTEX  DeploymentSpec SET key = "TITAN_DEPLOYMENTSPEC"
+CREATE VERTEX  Artifact SET key = "TITAN_JDBC"
+CREATE VERTEX  Artifact SET key = "TITAN_DB"
 
-CREATE EDGE Delivery_Instance FROM (SELECT FROM COMPONENT WHERE key = "TITAN") TO (SELECT FROM COMPONENT WHERE key = "DELIVERY_TITAN")
-CREATE EDGE Manifested_By FROM (SELECT FROM COMPONENT WHERE key = "TITAN") TO (SELECT FROM Artifact WHERE key = "TITAN_EAR")
-CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "TITAN_EAR") TO (SELECT FROM ExecutionEnvironment WHERE key = "TLB01")
-CREATE EDGE Deployed_To FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_TITAN") TO (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_TLB01")
-CREATE EDGE Has FROM (SELECT FROM COMPONENT WHERE key = "DELIVERY_TITAN") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_TITAN_DEPLOYMENTSPEC")
-CREATE EDGE Instance_Of FROM (Select FROM DeploymentSpec WHERE key = "TITAN_DEPLOYMENTSPEC") TO (Select FROM DeploymentSpec WHERE key = "DELIVERY_TITAN_DEPLOYMENTSPEC")
-CREATE EDGE Deployed_To FROM (Select FROM Artifact WHERE key = "TITAN_EAR") TO (Select FROM DeploymentSpec WHERE key = "TITAN_DEPLOYMENTSPEC")
+--Logical Components
+CREATE VERTEX LogicalComponent SET key = "TITAN_EAR"
+CREATE VERTEX LogicalComponent SET key = "TITAN_JDBC"
+CREATE VERTEX LogicalComponent SET key = "TITAN_DB"
 
+-- Define which Artifact manisfest which LogicalComponent
+CREATE EDGE Manifests FROM (SELECT FROM Artifact WHERE key = "TITAN_EAR") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR")
+CREATE EDGE Manifests FROM (SELECT FROM Artifact WHERE key = "TITAN_JDBC") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC")
+CREATE EDGE Manifests FROM (SELECT FROM Artifact WHERE key = "TITAN_DB") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_DB")
 
--- Database connection
-
-CREATE CLASS DatabaseConnection IF NOT EXISTS EXTENDS V 
-CREATE CLASS HAS_DATABASE IF NOT EXISTS EXTENDS E 
-
-CREATE VERTEX  DatabaseConnection SET key = "Conn1", user = "titan proxy", schema = "titan", database_instance = "OSDB", database_server = "Cluster1"
-CREATE EDGE HAS_DATABASE FROM (SELECT FROM ExecutionEnvironment WHERE key = "DELIVERY_TLB01") TO (SELECT FROM DatabaseConnection WHERE key = "Conn1")
+--CREATE EDGE Is_Used_By FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR")
+CREATE EDGE Uses FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC")
 
 
--- select * from `LogicalComponent` where key = "TITAN"
+-- Create Parent (top-level) component/bundle
+CREATE VERTEX LogicalComponent SET key = "TITAN"
+CREATE EDGE Has FROM (SELECT FROM LogicalComponent WHERE key = "TITAN") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR")
+CREATE EDGE Has FROM (SELECT FROM LogicalComponent WHERE key = "TITAN") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC")
+CREATE EDGE Has FROM (SELECT FROM LogicalComponent WHERE key = "TITAN") TO (SELECT FROM LogicalComponent WHERE key = "TITAN_DB")
+
+-- Physical Components
+CREATE VERTEX PhysicalComponent SET key = "TITAN_EAR_I"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_I")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_EAR_T"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_T")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_EAR_P"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_EAR") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_P")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_I") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1I")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_T") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1T")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_EAR_P") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1P")
+
+
+CREATE VERTEX PhysicalComponent SET key = "TITAN_JDBC_I"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_I")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_JDBC_T"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC") To (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_T")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_JDBC_P"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_JDBC") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_P")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_I") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1I")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_T") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1T")
+CREATE EDGE Deployed_To FROM (SELECT FROM PhysicalComponent WHERE key = "TITAN_JDBC_P") TO (SELECT FROM PhysicalExecutionEnvironment WHERE key = "WLS1P")
+
+
+CREATE VERTEX PhysicalComponent SET key = "TITAN_DB_I"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_DB") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_DB_I")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_DB_T"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_DB") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_DB_T")
+CREATE VERTEX PhysicalComponent SET key = "TITAN_DB_P"
+CREATE EDGE Has_Instance FROM (SELECT FROM LogicalComponent WHERE key = "TITAN_DB") TO (SELECT FROM PhysicalComponent WHERE key = "TITAN_DB_P")
+
+
+--CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "TITAN_EAR") TO (SELECT FROM V WHERE key = "WLS1")
+--CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "TITAN_JDBC") TO (SELECT FROM V WHERE key = "WLS1")
+--CREATE EDGE Deployed_To FROM (SELECT FROM Artifact WHERE key = "TITAN_DB") TO (SELECT FROM V WHERE key = "WLS1")
+
+--CREATE EDGE Deployed_To FROM (SELECT FROM V WHERE key = "TITAN_EAR_I") TO (SELECT FROM V WHERE key = "WLS1I")
+
+
